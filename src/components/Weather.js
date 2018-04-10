a59facdb5df4b61aa9deab0a7fbc86b45ab0c73c
Arthur Smith <arthursmith@ms-MacBook-Pro.local>
2018-04-09 19:11:20 -0500

    api is being called and printed, along with some styling needs being done

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,17 +3,52 @@ import './weather.css';
 
 
 
-
-
-function WeatherComponent (props) {
-    return (
-        <div className="text-center">
-            <h4>Temperature for your area:</h4>
-            <p key={props.name}>
-                {props.name}: {props.temp}
-            </p>
-        </div>
+function WeatherInPut(props) {
+    return(
+        <div className="container d-flex justify-content-center input-group input-group-sm mb-5 w-25">
+            <span>
+                <input id="input-field" type="text" placeholder="Enter Area Zipcode" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" /> 
+            </span>
+            
+        
+        {props.showResult ?
+            <div> </div>
+            :
+            <div className="input-group-prepend">
+                <span className="input-group-text" id="inputGroup-sizing-sm" onClick={() => { props.showClicked() }}>Enter
+                </span>
+                
+            </div>
+            
+        }                
+        </div>      
     )
+}
+
+
+
+
+function WeatherOutPut (props) {
+    if(props.showResult) {
+        return (
+            <div className="container text-center">
+                <h4>Temperature for your area:</h4>
+                <p key={props.name}>
+                    {props.name}: {props.temp}
+                </p>
+                <div className='d-flex justify-content-center mb-5'>
+                    <input id="input-field" type="text" className="w-25 form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" /> 
+
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id="inputGroup-sizing-sm" onClick={() => { props.showClicked() }}>Enter
+                        </span>
+                    </div>
+                </div>
+            </div>
+        )
+    }else {
+        return null;
+    }
 } 
 
 
@@ -28,21 +63,31 @@ class Weather extends React.Component {
             error: null,
             weatherLoaded: false,
             objResult: {},
+            showResult: false,
+            value:'',
         }
+
+        this.showClicked = this.showClicked.bind(this);
     }
 
-    componentDidMount() {
-        console.log('componentDidMount');
-        this.getWeather();
+
+    showClicked() {
+        let val = document.getElementById('input-field').value
+        this.setState({
+            value: val,
+            showResult:true
+        }, this.getWeather(val))
+
     }
 
-    getWeather() {
-        console.log("getWeather");
-        fetch("http://api.openweathermap.org/data/2.5/forecast?zip=37210,us&units=imperial&APPID=aeace81c0d72e15b17ad94207544e2f8")
+
+
+
+    getWeather(value) {
+        fetch(`http://api.openweathermap.org/data/2.5/forecast?zip=${value},us&units=imperial&APPID=aeace81c0d72e15b17ad94207544e2f8`)
         .then(data => data.json())
         .then(
             (result) => {
-                console.log('result from promise', result);
                 this.setState({
                     weatherLoaded: true,
                     objResult: result,
@@ -50,6 +95,7 @@ class Weather extends React.Component {
             },
             (error) => {
                 this.state ({
+                    showResult:true,
                     error: error,
                 });
             })
@@ -58,7 +104,7 @@ class Weather extends React.Component {
 
 
     render() {
-        const { error, weatherLoaded, objResult } = this.state;
+        const { error, weatherLoaded, objResult,showResult, value } = this.state;
 
         if (error) {
         return( 
@@ -70,16 +116,20 @@ class Weather extends React.Component {
         else if (!weatherLoaded) { 
         return ( 
             <div className="text-center">
-                Loading...
+            <WeatherInPut
+            weatherLoaded={weatherLoaded}
+            showClicked={this.showClicked}
+            value={value} />
             </div>
         )
         } else {
         return(
         <div>
-
-            <WeatherComponent
+            <WeatherOutPut
+            showResult={showResult}
             name={objResult.city.name}
-            temp={objResult.list[0].main.temp} />
+            temp={objResult.list[0].main.temp}
+            showClicked={this.showClicked} />
 
         </div>
             )
